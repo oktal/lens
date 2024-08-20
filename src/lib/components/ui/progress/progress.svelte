932b3e97ee307b29e6753a377b4ca353c74a5c0b
Mathieu Stefani <m.stefani@abc-arbitrage.com>
2024-08-20 09:20:10 +0200

    feat(ui): add indeterminate progress bar

--- src/lib/components/ui/progress/progress.svelte
+++ src/lib/components/ui/progress/progress.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
-	import { Progress as ProgressPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+	type Props = {
+		value?: number,
+		min?: number,
+		max?: number,
 
-	type $$Props = ProgressPrimitive.Props;
+		height?: number,
+	}
 
-	let className: $$Props["class"] = undefined;
-	export let max: $$Props["max"] = 100;
-	export let value: $$Props["value"] = undefined;
-	export { className as class };
+	let { value = undefined, min = 0, max = 100, height = 2 }: Props = $props();
+
+	let fillPercent = $derived(value ? (100 * (value - min)) / (max - min) : 0);
+	let indeterminate = $derived(value === undefined || value < 0);
+	let indeterminateClass = $derived(indeterminate ? 'anim-indeterminate': '');
+
+	const heightClass = `h-${height}`;
 </script>
 
-<ProgressPrimitive.Root
-	class={cn("bg-secondary relative h-4 w-full overflow-hidden rounded-full", className)}
-	{...$$restProps}
+<!-- Track -->
+<div
+	class="progress-bar w-full overflow-hidden bg-secondary {heightClass} rounded-full"
+	data-testid="progress-bar"
+	role="progressbar"
+	aria-valuenow={value}
+	aria-valuemin={min}
+	aria-valuemax={max - min}
 >
-	<div
-		class="bg-primary h-full w-full flex-1 transition-all"
-		style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}
-	></div>
-</ProgressPrimitive.Root>
+	<!-- Meter -->
+	<div class="progress-bar-meter h-full bg-primary rounded-full {indeterminateClass} transition-[width]" style:width="{indeterminate ? 100 : fillPercent}%"></div>
+</div>
+
+<style lang="postcss">
+	.anim-indeterminate {
+		transform-origin: 0% 50%;
+		animation: anim-indeterminate 2s infinite linear;
+	}
+	/* prettier-ignore */
+	@keyframes anim-indeterminate {
+		0% { transform: translateX(0) scaleX(0); }
+		40% { transform: translateX(0) scaleX(0.4); }
+		100% { transform: translateX(100%) scaleX(0.5); }
+	}
+</style>
