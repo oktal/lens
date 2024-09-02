2fe640262b89cdef5d636d20a62cb10a86fd769a
Mathieu Stefani <auquetal@gmail.com>
2024-09-02 16:10:32 +0200

    chore: add default catalog and schema

--- src-tauri/src/lens.rs
+++ src-tauri/src/lens.rs
@@ -48,7 +48,8 @@ impl Lens {
         // Setup session
         let config = SessionConfig::new()
             .with_information_schema(false)
-            .with_create_default_catalog_and_schema(false);
+            .with_create_default_catalog_and_schema(true)
+            .with_default_catalog_and_schema("datalens", "default");
 
         let ctx = SessionContext::new_with_config(config);
 
